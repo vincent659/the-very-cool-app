1f3c7e75f54190f088528260cc403acbac22a3e9
VincentC <vincent65961@gmail.com>
2020-10-15 17:17:02 +1000

    twitter stream; key extractiom; sentiment analysis

--- routes/api.js
+++ routes/api.js
@@ -1,79 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const MonkeyLearn = require('monkeylearn');
 const Twit = require('twit');
-
-// Obtaining News Articles
-router.post('/news', async (req, res) => {
-  try {
-    const queryToBeSearched = req.body.msg;
-    // two types of end points 'top-headlines' & 'everything'
-    const endpoint = 'top-headlines';
-    const country = 'us';
-
-    // NewsAPI Fetching
-    let config = {
-      headers: {
-        Cookie: '__cfduid=dbdf5e69aca6bbdc4dd657b7e5e5f1d661599108067',
-      },
-    };
-    const newsArticleRaw = await axios.get(
-      `https://newsapi.org/v2/${endpoint}?country=${country}&q=${queryToBeSearched}&apiKey=${process.env.NEWSAPI_API}`,
-      config
-    );
-
-    res.json({ msg: newsArticleRaw.data });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// Sentimental Analysis and Word Extraction Process
-router.post('/analysis', async (req, res) => {
-  try {
-    const newsArticleContent = req.body.msg;
-    //   MonkeyLearn Sentimental Analysis Processing
-    const ml = new MonkeyLearn(process.env.MONKEYLEARN_API);
-    let model_id = 'cl_pi3C7JiL';
-    let newsContent = [newsArticleContent];
-    resultSentiment = await ml.classifiers.classify(model_id, newsContent);
-
-    // TextRazor Keyword Extractor
-    const qs = require('qs');
-    let data = qs.stringify({
-      extractors: 'topics',
-      text: newsArticleContent,
-    });
-
-    let config = {
-      method: 'post',
-      url: 'https://api.textrazor.com',
-      headers: {
-        'x-textrazor-key': process.env.RAZORTEXT_API,
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      data: data,
-    };
-
-    let resultExtract = await axios(config);
-
-    res.json({
-      status: true,
-      dataSentiment: resultSentiment.body[0].classifications,
-      dataExtract: resultExtract.data.response.topics.slice(0, 11),
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+const natural = require('natural');
+const Analyzer = require('natural').SentimentAnalyzer;
+const stemmer = require('natural').PorterStemmer;
+const Sentiment = require('sentiment');
+const io = require('../socket');
 
 // Tweets Processing
 router.post('/tweets', async (req, res) => {
   try {
-    const keyWord = req.body.msg;
+    const keyWord = '#' + req.body.msg;
+
+    // console.log(keyWord);
 
     let T = new Twit({
       consumer_key: process.env.CONSUMER_KEY,
@@ -84,13 +24,73 @@ router.post('/tweets', async (req, res) => {
       strictSSL: true, // optional - requires SSL certificates to be valid.
     });
 
-    T.get(
-      'search/tweets',
-      { q: `${keyWord} since:2020-01-01`, count: 10 },
-      function (err, data, response) {
-        res.json(data);
-      }
-    );
+    // T.get(
+    //   'search/tweets',
+    //   { q: `${keyWord} since:2020-01-01`, count: 10 },
+    //   function (err, data, response) {
+    //     res.json(data);
+    //   }
+    // );
+
+    /* Twitter Stream Processing */
+    // track is the word that we want to track
+    var stream = T.stream('statuses/filter', {
+      track: `${keyWord}`,
+      language: 'en',
+    });
+
+    stream.on('tweet', function (tweet) {
+      // text of the tweet
+      console.log('Tweet: ' + tweet.text);
+      let tweetPosts = tweet.text;
+
+      console.log('Location: ' + tweet.user.location);
+      let tweetLocation = tweet.user.location;
+
+      /* Natural - Word Extraction Processing */
+      // natural.PorterStemmer.attach();
+      // tokenWords = tweetPosts.tokenizeAndStem();
+      // console.log(tokenWords);
+
+      /* Natural - Sentiment Analysis Processing */
+      // let analyzer = new Analyzer('English', stemmer, 'senticon');
+      // console.log(analyzer.getSentiment(tokenWords));
+      // console.log(natural.PorterStemmer.stem('i like to play harding'));
+
+      /* Sentiment - Sentiment Analysis Processing */
+      let sentiment = new Sentiment();
+      let result = sentiment.analyze(tweetPosts);
+      console.log(result); // Score: -2, Comparative: -0.666
+
+      // res.json(tweet);
+    });
+
+    // let analyzer = new Analyzer('English', stemmer, 'afinn');
+
+    // console.log(analyzer.getSentiment(['I', 'hate', 'cherries']));
+    // console.log(natural.PorterStemmer.stem('i like to play harding'));
+
+    // Establishes socket connection
+    // io.on('connection', (socket) => {
+    //   getApiAndEmit(socket);
+    //   // stream.on('tweet', function (tweet) {
+    //   //   res.json(tweet);
+    //   //   // console.log(tweet);
+    //   // });
+    //   socket.on('connection', () => console.log('Client connected'));
+    //   socket.on('disconnected', () => console.log('Client disconnected'));
+    // });
+
+    // const getApiAndEmit = async (socket) => {
+    //   try {
+    //     stream.on('tweet', function (tweet) {
+    //       res.json(tweet);
+    //     });
+    //     socket.emit('FromAPI', res.data); // Emitting a new message. It will be consumed by the client
+    //   } catch (error) {
+    //     console.error(`Error: ${error.code}`);
+    //   }
+    // };
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
